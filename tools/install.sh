89afdd1da4533a5ba4f390382c9a961827bc1416
swapnilmj <2190589+swapnilmj@users.noreply.github.com>
2019-01-04 12:48:21 +0530

    Allowing offline/standalone installation

--- tools/install.sh
+++ tools/install.sh
@@ -33,11 +33,11 @@ main() {
     ZSH=~/.oh-my-zsh
   fi
 
-  if [ -d "$ZSH" ]; then
-    printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
-    printf "You'll need to remove $ZSH if you want to re-install.\n"
-    exit
-  fi
+  #if [ -d "$ZSH" ]; then
+    #printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
+    #printf "You'll need to remove $ZSH if you want to re-install.\n"
+    #exit
+  #fi
 
   # Prevent the cloned repository from having insecure permissions. Failing to do
   # so causes compinit() calls to fail with "command not found: compdef" errors
@@ -59,10 +59,10 @@ main() {
       exit 1
     fi
   fi
-  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
-    printf "Error: git clone of oh-my-zsh repo failed\n"
-    exit 1
-  }
+  #env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
+    #printf "Error: git clone of oh-my-zsh repo failed\n"
+    #exit 1
+  #}
 
 
   printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
